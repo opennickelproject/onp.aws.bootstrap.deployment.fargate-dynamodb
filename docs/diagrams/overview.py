b16585b51388f318994e5d6c0dd4110542d1fcbc
Dishan <dishan@newtonrussell.com>
2022-11-06 18:49:23 +0000

    Updated image path

--- docs/diagrams/overview.py
+++ docs/diagrams/overview.py
@@ -4,5 +4,5 @@ from diagrams.aws.security import IAMRole
 from diagrams.aws.compute import Fargate
 from diagrams.aws.database import Dynamodb
 
-with Diagram("Fargate DynamoDB Pattern Overview", show=False, direction="TB", filename='res/overview', outformat='png'):
+with Diagram("Fargate DynamoDB Pattern Overview", show=False, direction="TB", filename='docs/diagrams/res/overview', outformat='png'):
     IAMRole("DynamoDB Access Role") >> Fargate("Service")>> Dynamodb("Dynamodb Table")
